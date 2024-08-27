900ab3a67f629cb49a14c3273017c34941e92148
cristian <cchura.cpc@gmail.com>
2024-08-26 22:31:00 -0400

    pedidos

--- app/Http/Controllers/PedidoController.php
+++ app/Http/Controllers/PedidoController.php
@@ -2,16 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pedido;
+use App\Models\Producto;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PedidoController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        // $fi = $request->fecha_inicio;
+        // $ff = $request->fecha_fin;
+
+        $pedidos = Pedido::with('productos', 'cliente')->get();
+
+        return response()->json($pedidos);
     }
 
     /**
@@ -19,7 +27,38 @@ class PedidoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // validar
+        $request->validate([
+            "cliente_id" => "required"
+        ]);
+
+        DB::beginTransaction();
+
+        try {
+
+            $productos = $request["productos"];
+
+            $pedido = new Pedido();
+            $pedido->fecha_pedido = date("Y-m-d H:i:s");
+            $pedido->estado = $request->estado;
+            $pedido->cliente_id = $request->cliente_id;
+            $pedido->observaciones = $request->observaciones;
+            $pedido->save();
+
+            // guardar
+            foreach ($productos as $prod) {
+                $id_prod = $prod['id'];
+                $cant_prod = $prod['cantidad'];
+
+                $pedido->productos()->attach($id_prod, ["cantidad" => $cant_prod]);
+            }
+
+            DB::commit();
+            // all good
+        } catch (\Exception $e) {
+            DB::rollback();
+            // something went wrong
+        }
     }
 
     /**
